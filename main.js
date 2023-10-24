f62ec6561ca47efc808d2396938c06e7319d3ad2
Niloofar Keshavarz <niloofar.keshavarz@gmail.com>
2023-10-24 12:42:35 -0400

    compiles ts file to js

--- main.js
+++ main.js
@@ -11,3 +11,19 @@
 //     console.log("Finally " + i);
 // };
 // doSth();
+var a;
+a = 1;
+var b;
+var c;
+var d;
+var e;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+; //best practice to add numbers for furthur modification
+var backgroundColor = Color.Green;
+console.log(a);
