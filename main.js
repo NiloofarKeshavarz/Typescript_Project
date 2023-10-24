03eb909afefd9399ef9b0569ed1d78a5240b9f7e
Niloofar Keshavarz <niloofar.keshavarz@gmail.com>
2023-10-24 11:47:40 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// function log(message){
+//     console.log(message);
+// };
+// var message = 'Hello World';
+// log(message);
+function doSth() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally" + i);
+}
+;
+doSth();
